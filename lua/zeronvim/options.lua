34b43486f9bca6a0f2636613f2e33abe65aee288
zettlrobert <zettl.robert@gmail.com>
2022-09-24 02:03:30 +0200

    feat: adding base options

--- lua/zeronvim/options.lua
+++ lua/zeronvim/options.lua
@@ -14,8 +14,27 @@
 -- fileencoding - file-content encoding for the current buffer
 -- hlsearch - when there is a previous search pattern highlight all its matches
 -- ignorecase - ignore case in search patterns
+-- laststatus - influences when the last window will have a status line
 -- mouse - enable mouse support
---
+-- number - print the line number in front of each line.
+-- numberwidth - number of columns to use for the line number
+-- pumheight - maximum number of items to show in popup menu
+-- relativenumber - show the line number relative to the line with the cursor
+-- scrolloff - minimal number of screen lines to keep above and below the cursor
+-- shiftwidth - number of spaces to use for each step of (auto)indent
+-- showtabline - the value of this option specifies when the line with tab page labels will be displayed
+-- sidescrolloff - the minimal number of screen columns to keep to the left and to the right of the cursor if nowrap is set
+-- signcolumn - when and how to draw the signcolumn
+-- smartcase - override the ignorecase option if the search patter contains upper case characters
+-- smartindent - do smart autoindenting when starting a new line
+-- splitbelow - when on, splitting a window will put the new window below the current
+-- splitright - when on, splitting a window will put the new window to the right of the current one
+-- swapfile - use a swapfile for the buffer
+-- tabstop - number of spaces that a tab in the file counts for
+-- termguicolors - enables 24-bit RGB color in the TUI
+-- undofile - the name of the undo file that would be used for a file with {name} when writing
+-- updatetime - if this many millisecond's nothing is typed the swap file will be written to disk 
+-- wrap - this option changes how text is displayed
 --]]
 local options = {
   autoindent = true,
@@ -33,41 +52,66 @@ local options = {
   fileencoding = "utf-8",
   hlsearch = true,
   ignorecase = true,
+  laststatus = 3,
   mouse = 'a',
-
-
+  number = true,
+  numberwidth = "5",
+  pumheight = 10,
+  relativenumber = true,
+  scrolloff = 16,
+  shiftwidth = 2,
+  showtabline = 2,
+  sidescrolloff = 16,
+  signcolumn = "yes",
+  smartcase = true,
+  smartindent = true,
+  splitbelow = true,
+  splitright = true,
+  swapfile = false,
+  tabstop = 4,
+  termguicolors = true,
+  undofile = true,
+  updatetime = 300,
+  wrap = true
 }
+
+-- Set option with value from options array
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
+
 
 -- TODO: evaluate preview for no completeopt
 -- TODO: foldclose, foldcolumn, foldenable
 -- TODO: is guifont needed?
 -- TODO: icon, iconstring
 -- TODO: langmap
--- TODO: linebreak
+-- TODO: linebreak - this has to be used in conjunction with wrap
+-- TODO: wrap
+-- TODO: scrollback
+-- TODO: showbreak
+-- TODO: spellsuggest
+-- TODO: statusline
+-- TODO: switchbuf
+-- TODO: tabline
+--
+--[[]]
+--[[ vim.opt.shortmess:append "c" 				    -- Don't pass messages to |ins-completion-menu|. ]]
+--[[ vim.opt.errorbells=false                       	-- Disable soundeffects ]]
+--[[ vim.cmd('syntax on')                         	-- Syntax on ]]
+--[[ vim.o.hidden = true                             -- Required to keep multiple buffers open multiple buffers ]]
+--[[ vim.o.mousemodel = "popup"			            -- Sets the model for the mouse to use ]]
+--[[ vim.cmd('highlight Normal guibg=none') ]]
+--[[]]
+--[[]]
+--[[ --  Tab, Indent and Words ]]
+--[[ vim.o.softtabstop = 4                           -- Insert 4 spaces for a tab ]]
+--[[ vim.bo.autoindent = true                        -- Copy indent from current line ]]
+--[[ vim.cmd('set iskeyword+=-')       	            -- treat dash separated words as a word text object" ]]
+--[[ vim.cmd('set shortmess+=c') ]]
+--[[ vim.cmd('set inccommand=split')			        -- Make substitution work in real time ]]
+--[[ -- vim.o.spell = true				            -- Spell checking for nvim enabled ]]
+--[[ vim.o.spellcapcheck = ''			            -- Ignore capitalization when spell checking ]]
+--[[ vim.o.showmatch = true				            -- When bracket is inserted, briefly jump to the matching one. ]]
+--[[ vim.o.spelllang = 'en_us'                       -- Languages ]]
 
-
---[[
-
-
-
-  laststatus = 3,                               -- status line only on last window
-  pumheight = 10, 					            -- Makes popup menu height
-  showtabline = 2, 					            -- Always show tabs
-  smartcase = true, 					        -- Smartcase
-  smartindent = true,                        	-- Makes indenting smart
-  splitbelow = true,                       	    -- Horizontal splits will automatically be below
-  splitright = true,                       	    -- Vertical splits will automatically be to the right
-  swapfile = false,                          	-- No swap file
-  termguicolors = true,                     	-- set term GUI colors most terminals support this
-  cndofile = true,					            -- enable persistend uindo
-  updatetime = 300,			                    -- Faster update time.
-  shiftwidth = 2,   				            -- Shiftwidth value
-  tabstop = 4, 					                -- Insert 4 spaces for a tab
-  number = true,                          		-- Set numbered lines
-  relativenumber = true,	                  	-- Set relative line numbers
-  numberwidth = 4, 				                -- set number column width to (default 4)
-  cigncolumn = "yes",  	                   	    -- Always show the signcolumn, otherwise it would shift the text each time
-  wrap = false,                             	-- Display long lines as just one line
-  scrolloff = 16,           	                -- Move up and down when reaching + - 16 lines of code
-  sidescrolloff = 8,				            -- The minimal numer of screenoffset to keep to the left and right
-- ]]
