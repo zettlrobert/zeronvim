6b6d4f3dadd8991eeb530814dc0fa951edfecac1
zettlrobert <zettl.robert@gmail.com>
2022-10-22 12:25:25 +0200

    feat(keymaps): resize buffer with arrow keys

--- lua/zeronvim/keymaps.lua
+++ lua/zeronvim/keymaps.lua
@@ -20,3 +20,9 @@ keymap("n", "<C->h", "<C-w>h", opts)
 keymap("n", "<C->j", "<C-w>j", opts)
 keymap("n", "<C->k", "<C-w>k", opts)
 keymap("n", "<C->l", "<C-w>l", opts)
+
+-- Resize Buvver with arrow keys
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
