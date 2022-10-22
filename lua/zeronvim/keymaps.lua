9585c9f561f75827ec9eee4a3d97e32d4c9b88f9
zettlrobert <zettl.robert@gmail.com>
2022-10-22 12:21:58 +0200

    feat(keymaps): move arround buffers

--- lua/zeronvim/keymaps.lua
+++ lua/zeronvim/keymaps.lua
@@ -14,3 +14,9 @@ vim.g.maplocalleader = " "
 
 -- Reload Configuration
 keymap("n", "<Leader>sv", "<cmd>lua require'zeronvim.utils.sourcing'.source_nvim_configuration()<CR>", opts)
+
+-- Window Navigation
+keymap("n", "<C->h", "<C-w>h", opts)
+keymap("n", "<C->j", "<C-w>j", opts)
+keymap("n", "<C->k", "<C-w>k", opts)
+keymap("n", "<C->l", "<C-w>l", opts)
