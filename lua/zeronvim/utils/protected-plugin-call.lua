319aa81302be97ed97ecf1bce60a022573bfa1d7
zettlrobert <zettl.robert@gmail.com>
2022-11-12 18:14:10 +0100

    feat: function to make protected calls to plugins

---
+++ lua/zeronvim/utils/protected-plugin-call.lua
@@ -0,0 +1,12 @@
+-- Function to call plugins in a protected way availalbe with custom utils
+local function protectedPluginCall(plugin)
+  local status_ok, initializedPlugin = pcall(require, plugin)
+  if not status_ok then
+    vim.notify("ERROR: Could not require " .. plugin
+    return
+  else
+    return initializedPlugin
+  end
+end
+
+return protectedPluginCall
