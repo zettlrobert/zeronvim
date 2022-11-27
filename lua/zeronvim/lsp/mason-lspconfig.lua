dbba93b46713109fb2a06271309701d2dffbe4f8
zettlrobert <zettl.robert@gmail.com>
2022-11-27 20:29:30 +0100

    feat: first draft working setup

--- lua/zeronvim/lsp/mason-lspconfig.lua
+++ lua/zeronvim/lsp/mason-lspconfig.lua
@@ -27,17 +27,28 @@ if mason_lspconfig and lspconfig and handlers then
   -- list of all mason installed langauge servers
   local installed_servers = mason_lspconfig.get_installed_servers()
 
+  local sumneko_lua_config = require "zeronvim.lsp.providers.sumneko_lua"
+
   -- Loop over all availalbe langauge servers, and use mason default setup
   local function server_setup()
-      for _, server in pairs(installed_servers) do
-        lspconfig[server].setup{
-          on_attach = handlers.on_attach
+    for _, server in pairs(installed_servers) do
+      lspconfig[server].setup{
+        on_attach = handlers.on_attach
+      }
+
+      -- Override defaults for server
+      if server == "sumneko_lua" then
+        lspconfig.sumneko_lua.setup {
+          on_attach = handlers.on_attach,
+          settings = sumneko_lua_config
         }
       end
+
+    end
   end
 
   -- advanced feature to setup servers automatically
   mason_lspconfig.setup_handlers {
-    server_setup
+    server_setup,
   }
 end
