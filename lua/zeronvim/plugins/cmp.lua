2a199d4feb567d3550cd0b9fc5279f91a6402d76
zettlrobert <zettl.robert@gmail.com>
2022-12-09 21:33:31 +0100

    refactor: add keyword after lsp kind icon

--- lua/zeronvim/plugins/cmp.lua
+++ lua/zeronvim/plugins/cmp.lua
@@ -92,7 +92,7 @@ if cmp and luasnip then
       { name = "luasnip" },
       { name = "buffer" },
       { name = "treesitter" },
-      { name = "omni" },  -- WARN: could slow down
+      { name = "omni" },
       { name = "nvim_lsp_signature_help" },
       { name = "conventionalcommits" }
       --      { name = "spell" },
@@ -105,9 +105,7 @@ if cmp and luasnip then
     formatting = {
       fields = { "kind", "abbr", "menu" },
       format = function(entry, vim_item)
-        -- Kind icons
-        vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-        -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+        vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind)
         vim_item.menu = ({
           nvim_lsp = "[LSP]",
           nvim_lua = "[NVIM_LUA]",
