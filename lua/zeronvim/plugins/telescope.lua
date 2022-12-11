765e8bdd95330a5b134e78bedfa656f212c4ee4a
zettlrobert <zettl.robert@gmail.com>
2022-12-11 16:00:20 +0100

    fix: remove pickers as nvim-tree complians about them

--- lua/zeronvim/plugins/telescope.lua
+++ lua/zeronvim/plugins/telescope.lua
@@ -2,11 +2,5 @@ local utils = require "zeronvim.utils"
 local telescope = utils.protected_plugin_call("nvim-tree")
 
 if telescope then
-  telescope.setup({
-    pickers = {
-      find_files = {
-        theme = "ivy"
-      }
-    }
-  })
+  telescope.setup({})
 end
