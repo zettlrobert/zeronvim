d9f6eb920c0b621d0f56ac304ed01cd8feda69a7
zettlrobert <zettl.robert@gmail.com>
2022-12-11 03:38:51 +0100

    feat: add plugins Comment and nvim-ts-context-commentstring

--- lua/zeronvim/plugins/treesitter.lua
+++ lua/zeronvim/plugins/treesitter.lua
@@ -2,30 +2,39 @@ local utils = require "zeronvim.utils"
 local treesitter_configs = utils.protected_plugin_call("nvim-treesitter.configs") 
 
 if treesitter_configs then
-treesitter_configs.setup {
-  -- A list of parser names, or "all"
-  ensure_installed = "all",
+  treesitter_configs.setup {
+    -- A list of parser names, or "all"
+    ensure_installed = "all",
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
 
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
+    -- Automatically install missing parsers when entering buffer
+    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    auto_install = true,
 
-  highlight = {
-    enable = true,
+    autopairs = {
+      enable = true
+    },
 
-    -- list of language that will be disabled
-    disable = { "" },
+    highlight = {
+      enable = true,
 
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
+      -- list of language that will be disabled
+      disable = { "" },
 
-  indent = { enable = true },
-}
+      -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+      -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+      -- Using this option may slow down your editor, and you may see some duplicate highlights.
+      -- Instead of true it can also be a list of languages
+      additional_vim_regex_highlighting = false,
+    },
+
+    indent = { enable = true },
+
+    context_commentstring = {
+      enable = true,
+      enablue_autocmd  = false
+    }
+  }
 end
