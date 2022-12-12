8f841a1fa791bea76f740f77edb091ce4d105a25
zettlrobert <zettl.robert@gmail.com>
2022-12-12 03:13:16 +0100

    feat: add get-buf-option function

---
+++ lua/zeronvim/utils/get-buf-option.lua
@@ -0,0 +1,11 @@
+-- Function to get a buffer option value -- nvim_buf_get_option
+local function get_buf_option(opt)
+  local status_ok, buf_option = pcall(vim.api.nvim_buf_get_option, 0, opt)
+  if not status_ok then
+    return nil
+  else
+    return buf_option
+  end
+end
+
+return get_buf_option
