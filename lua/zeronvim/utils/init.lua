999ea7385797fa889a08346dcdd1b270047e7052
zettlrobert <zettl.robert@gmail.com>
2022-12-14 00:43:55 +0100

    feat: add utility functions to export

--- lua/zeronvim/utils/init.lua
+++ lua/zeronvim/utils/init.lua
@@ -6,6 +6,8 @@ utils.get_filename = require "zeronvim.utils.get-filename"
 utils.get_file_extension = require "zeronvim.utils.get-file-extension"
 utils.is_input_empty = require "zeronvim.utils.is-input-empty"
 utils.get_buf_option = require "zeronvim.utils.get-buf-option"
+utils.merge_tables = require "zeronvim.utils.merge-tables"
+utils.spelling = require "zeronvim.utils.spelling"
 
 -- Expose Module to configuration
 return utils
