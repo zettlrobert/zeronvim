31725d8b64bbe64be895d68e35fdbcaed9acc327
zettlrobert <zettl.robert@gmail.com>
2022-12-19 22:56:12 +0100

    feat: add ccc plugin

--- lua/zeronvim/plugins/ccc.lua
+++ lua/zeronvim/plugins/ccc.lua
@@ -1,9 +1,8 @@
 local utils = require "zeronvim.utils"
 local ccc = utils.protected_plugin_call("ccc")
 
-
 -- This Auto Command Toggles the CccHighligher when we enter and leave a buffer
-vim.api.nvim_create_autocmd({ "BufWinEnter", "BufWinLeave"}, {
+vim.api.nvim_create_autocmd({ "BufWinEnter", "BufWinLeave" }, {
   callback = function()
     vim.api.nvim_command('CccHighlighterToggle')
   end
@@ -11,4 +10,4 @@ vim.api.nvim_create_autocmd({ "BufWinEnter", "BufWinLeave"}, {
 
 if ccc then
   ccc.setup({})
-end 
+end
