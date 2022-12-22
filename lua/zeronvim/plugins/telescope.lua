0c0901d911d3a31bb9a064826e28471ec7eabc04
zettlrobert <zettl.robert@gmail.com>
2022-12-23 00:50:01 +0100

    feat: add base telescope configuration

--- lua/zeronvim/plugins/telescope.lua
+++ lua/zeronvim/plugins/telescope.lua
@@ -1,6 +1,27 @@
 local utils = require "zeronvim.utils"
-local telescope = utils.protected_plugin_call("nvim-tree")
+local telescope = utils.protected_plugin_call("telescope")
+local telescope_builtin = utils.protected_plugin_call("telescope.builtin")
+local M = {}
 
 if telescope then
-  telescope.setup({})
+  vim.notify("Telescope Config Loaded...")
+
+  telescope.setup {
+    defaults = {
+      path_display = { "smart" },
+    },
+    pickers = {
+      find_files = {
+        find_command = {'rg' ,'--hidden','--files' },
+      }
+    }
+  }
 end
+
+M.find_env = function()
+  if telescope_builtin then
+    telescope_builtin.find_files({no_ignore=true})
+  end
+end
+
+return M
