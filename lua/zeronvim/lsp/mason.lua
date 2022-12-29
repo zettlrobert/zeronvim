6b7ee9124088c32af58d33900eee1bf450669e9a
zettlrobert <zettl.robert@gmail.com>
2022-12-29 15:40:48 +0100

    feat: add chrome and firefox debug adpater auto install option

--- lua/zeronvim/lsp/mason.lua
+++ lua/zeronvim/lsp/mason.lua
@@ -38,7 +38,9 @@ if mason and mason_nvim_dap then
   -- Ensure we can install debug adapters with mason
   mason_nvim_dap.setup({
     ensure_installed = {
-      "js-debug-adapter"
+      "js-debug-adapter",
+      "chrome-debug-adapter",
+      "firefox-debug-adapter",
     },
     -- NOTE: this is left here for future porting in case needed
     -- Whether adapters that are set up (via dap) should be automatically installed if they're not already installed.
@@ -48,7 +50,7 @@ if mason and mason_nvim_dap then
     --   - true: All adapters set up via dap are automatically installed.
     --   - { exclude: string[] }: All adapters set up via mason-nvim-dap, except the ones provided in the list, are automatically installed.
     --       Example: automatic_installation = { exclude = { "python", "delve" } }
-    automatic_installation = false,
+    automatic_installation = true,
 
     -- Whether adapters that are installed in mason should be automatically set up in dap.
     -- Removes the need to set up dap manually.
@@ -58,7 +60,7 @@ if mason and mason_nvim_dap then
     -- 	- false: Dap is not automatically configured.
     -- 	- true: Dap is automatically configured.
     -- 	- {adapters: {ADAPTER: {}, }, configurations: {ADAPTER: {}, }}. Allows overriding default configuration.
-    automatic_setup = true,
+    automatic_setup = false,
   })
 end
 
