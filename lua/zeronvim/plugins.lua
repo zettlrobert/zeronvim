0343a79ad5683287902c5a5cbc3c7865d460a59b
zettlrobert <zettl.robert@gmail.com>
2023-04-04 01:28:40 +0200

    feat: add codeim plugin

--- lua/zeronvim/plugins.lua
+++ lua/zeronvim/plugins.lua
@@ -13,86 +13,100 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	-- Pleneary Lua Functions to use with neoviom
-	"nvim-lua/plenary.nvim",
+  -- Pleneary Lua Functions to use with neoviom
+  "nvim-lua/plenary.nvim",
 
-	-- Treesitter
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate"
-	},
+  -- Treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate"
+  },
 
-	"JoosepAlviste/nvim-ts-context-commentstring",
+  "JoosepAlviste/nvim-ts-context-commentstring",
 
-	-- Colorscheme
-	{ "catppuccin/nvim", name = "catppuccin" },
+  -- Colorscheme
+  { "catppuccin/nvim", name = "catppuccin" },
 
-	-- Lualine
-	"nvim-lualine/lualine.nvim",
+  -- Lualine
+  "nvim-lualine/lualine.nvim",
 
-	-- NvimTree - Filemanager
-	"nvim-tree/nvim-tree.lua",
+  -- NvimTree - Filemanager
+  "nvim-tree/nvim-tree.lua",
 
-	-- Indentlines
-	"lukas-reineke/indent-blankline.nvim",
+  -- Indentlines
+  "lukas-reineke/indent-blankline.nvim",
 
-	-- Icons
-	"nvim-tree/nvim-web-devicons",
+  -- Icons
+  "nvim-tree/nvim-web-devicons",
 
-	-- Gitsigns
-	"lewis6991/gitsigns.nvim",
+  -- Gitsigns
+  "lewis6991/gitsigns.nvim",
 
-	-- Comments
-	"numToStr/Comment.nvim",
+  -- Comments
+  "numToStr/Comment.nvim",
 
-	-- Telescope Fuzzy Finder
-	"nvim-telescope/telescope.nvim",
+  -- Telescope Fuzzy Finder
+  "nvim-telescope/telescope.nvim",
 
-	-- ToggleTerm
-	"akinsho/toggleterm.nvim",
+  -- ToggleTerm
+  "akinsho/toggleterm.nvim",
 
-	-- LSP
-	"williamboman/mason.nvim",
-	"williamboman/mason-lspconfig.nvim",
-	"neovim/nvim-lspconfig",
+  -- LSP
+  "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
+  "neovim/nvim-lspconfig",
 
-	-- Null-LS
-	"jose-elias-alvarez/null-ls.nvim",
+  -- Null-LS
+  "jose-elias-alvarez/null-ls.nvim",
 
-	-- Winbar, breadcrumbs
-	"SmiteshP/nvim-navic",
+  -- Winbar, breadcrumbs
+  "SmiteshP/nvim-navic",
 
-	-- CMP Completion
-	"hrsh7th/cmp-nvim-lsp",
-	"hrsh7th/cmp-path",
-	"hrsh7th/cmp-cmdline",
-	"hrsh7th/nvim-cmp",
-	"hrsh7th/cmp-nvim-lua",
-	"hrsh7th/cmp-buffer",
-	"saadparwaiz1/cmp_luasnip",
-	"hrsh7th/cmp-calc",
-	"f3fora/cmp-spell",
+  -- CMP Completion
+  "hrsh7th/cmp-nvim-lsp",
+  "hrsh7th/cmp-path",
+  "hrsh7th/cmp-cmdline",
+  "hrsh7th/nvim-cmp",
+  "hrsh7th/cmp-nvim-lua",
+  "hrsh7th/cmp-buffer",
+  "saadparwaiz1/cmp_luasnip",
+  "hrsh7th/cmp-calc",
+  "f3fora/cmp-spell",
 
-	-- Snippet Engine
-	"L3MON4D3/LuaSnip",
+  -- Snippet Engine
+  "L3MON4D3/LuaSnip",
 
-	-- Snippets
-	"rafamadriz/friendly-snippets",
+  -- Snippets
+  "rafamadriz/friendly-snippets",
 
-	-- Debugging
-	"jayp0521/mason-nvim-dap.nvim",
-	"mfussenegger/nvim-dap",
-	"rcarriga/nvim-dap-ui",
-	"theHamsta/nvim-dap-virtual-text",
-	"mxsdev/nvim-dap-vscode-js",
+  -- Debugging
+  "jayp0521/mason-nvim-dap.nvim",
+  "mfussenegger/nvim-dap",
+  "rcarriga/nvim-dap-ui",
+  "theHamsta/nvim-dap-virtual-text",
+  "mxsdev/nvim-dap-vscode-js",
 
-	-- Quickfix and Location List
-	"folke/trouble.nvim",
-	"folke/todo-comments.nvim",
+  -- Quickfix and Location List
+  "folke/trouble.nvim",
+  "folke/todo-comments.nvim",
 
-	-- Color Picker
-	"uga-rosa/ccc.nvim",
+  -- Color Picker
+  "uga-rosa/ccc.nvim",
 
-	-- Syntax
-	"jxnblk/vim-mdx-js"
+  -- Syntax
+  "jxnblk/vim-mdx-js",
+
+  -- AI Codeium
+  {
+    "jcdickinson/codeium.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "hrsh7th/nvim-cmp",
+    },
+    config = function()
+      require("codeium").setup(
+        require "codeium".config
+      )
+    end
+  },
 })
