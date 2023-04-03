3af9229a1d628a691db6e0e1520c5b3760cd93e5
zettlrobert <zettl.robert@gmail.com>
2023-04-04 00:09:02 +0200

    reactor: toggleterm options refactored

--- lua/zeronvim/keymaps.lua
+++ lua/zeronvim/keymaps.lua
@@ -136,13 +136,13 @@ keymap("n", "[t", todo_comments.todo_comments.jump_prev, { silent = true, desc =
 
 -- Toggleterm
 function _G.set_terminal_keymaps()
-  local opts = { buffer = 0 }
-  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+  local term_opts = { buffer = 0 }
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], term_opts)
+  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], term_opts)
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], term_opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], term_opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], term_opts)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], term_opts)
 end
 
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
