c117ebd9447fefe8108a642ec45611ed1eade773
zettlrobert <zettl.robert@gmail.com>
2023-04-05 01:26:25 +0200

    feat: plugins.lua --> lazy-plugins.lua

--- lua/zeronvim/plugins/codeium.lua
+++ lua/zeronvim/plugins/codeium.lua
@@ -1,7 +1,7 @@
 local M = {}
 local utils = require "zeronvim.utils"
 local codeium = utils.protected_plugin_call("codeium")
-local codeium_api_key = vim.fn.stdpath("data") .. "/codeium/api-key"
+local codeium_config = vim.fn.stdpath("data") .. "/codeium/codeium.json"
 local codeium_directory = vim.fn.stdpath("data") .. "/codeium"
 local language_server = codeium_directory .. "language-server-v1.1.32"
 local tool_uname_binary = "/usr/bin/uname"
@@ -9,10 +9,18 @@ local tool_uuidgen_binary = "/usr/bin/uuidgen"
 local tool_curl_binary = "/usr/bin/curl"
 local tool_gzip_binary = "/usr/bin/gzip"
 
+-- Autocommand to disable codeium by default on neovim startup
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    utils.toggle_codeium.disable()
+  end
+}
+)
+
 M.config = function()
   if codeium then
     return {
-      config_path = codeium_api_key,
+      config_path = codeium_config,
       bin_path = codeium_directory,
       tools = {
         uname = tool_uname_binary,
@@ -30,10 +38,9 @@ end
 return M
 
 -- TODO: Download Script for offical language server https://github.com/Exafunction/codeium/releases/tag/language-server-v1.1.32
--- TODO: Create a toggle to enable disable AI
 -- TODO: Create a statusline indicator combine with https://github.com/zettlrobert/zeronvim/issues/90
--- TODO: Create a funtion to quickly open the codeium log file in a new tab
 -- TODO: Ensure _ALL_ keybinds work or are rebound
+-- TODO: Unfortunately the plugin does not respect the global codeium_enabled variable
 --[
 --Defaults
 -- Clear current suggestion     - codeium#Clear()               <C-]>
