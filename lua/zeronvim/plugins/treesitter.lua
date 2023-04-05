dbbdb3468a320aa31a2f7fd3882c167205e10a68
zettlrobert <zettl.robert@gmail.com>
2023-04-06 00:44:34 +0200

    feat: add nvim-treesitter-context

--- lua/zeronvim/plugins/treesitter.lua
+++ lua/zeronvim/plugins/treesitter.lua
@@ -1,5 +1,6 @@
 local utils = require "zeronvim.utils"
-local treesitter_configs = utils.protected_plugin_call("nvim-treesitter.configs") 
+local treesitter_configs = utils.protected_plugin_call("nvim-treesitter.configs")
+local treesitter_context = utils.protected_plugin_call("treesitter-context")
 
 if treesitter_configs then
   treesitter_configs.setup {
@@ -19,10 +20,8 @@ if treesitter_configs then
 
     highlight = {
       enable = true,
-
       -- list of language that will be disabled
       disable = { "" },
-
       -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
       -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
       -- Using this option may slow down your editor, and you may see some duplicate highlights.
@@ -33,8 +32,22 @@ if treesitter_configs then
     indent = { enable = true },
 
     context_commentstring = {
-      enable = true,
-      enablue_autocmd  = false
+      enable          = true,
+      enablue_autocmd = false
     }
+  }
+end
+
+if treesitter_context then
+  treesitter_context.setup {
+    enable = true,
+    max_lines = 0,
+    min_window_height = 0,
+    line_numbers = true,
+    multiline_threshold = 20,
+    trim_scope = 'outer',
+    mode = 'cursor',
+    separator = nil,
+    zindex = 20,
   }
 end
