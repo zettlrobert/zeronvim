ba8f33d904911444a21ebaa9cefa5b28e87310bc
zettlrobert <zettl.robert@gmail.com>
2023-10-17 00:19:31 +0200

    feat: add get system binary utiltiy function and implement in codeium setup

--- lua/zeronvim/plugins/codeium.lua
+++ lua/zeronvim/plugins/codeium.lua
@@ -3,11 +3,12 @@ local utils = require "zeronvim.utils"
 local codeium = utils.protected_plugin_call("codeium")
 local codeium_config = vim.fn.stdpath("data") .. "/codeium/codeium.json"
 local codeium_directory = vim.fn.stdpath("data") .. "/codeium"
-local language_server = codeium_directory .. "language-server-v1.1.32"
-local tool_uname_binary = "/usr/bin/uname"
-local tool_uuidgen_binary = "/usr/bin/uuidgen"
-local tool_curl_binary = "/usr/bin/curl"
-local tool_gzip_binary = "/usr/bin/gzip"
+local language_server = codeium_directory .. "language_server_linux_x64"
+
+local tool_uname_binary = utils.get_system_binary("uname")
+local tool_uuidgen_binary = utils.get_system_binary("uuidgen")
+local tool_curl_binary = utils.get_system_binary("curl")
+local tool_gzip_binary = utils.get_system_binary("gzip")
 
 -- Autocommand to disable codeium by default on neovim startup
 vim.api.nvim_create_autocmd("VimEnter", {
@@ -38,9 +39,14 @@ end
 return M
 
 -- TODO: Download Script for offical language server https://github.com/Exafunction/codeium/releases/tag/language-server-v1.1.32
+-- create utility that checks if server binary is avialable and downloads it if not
+
 -- TODO: Create a statusline indicator combine with https://github.com/zettlrobert/zeronvim/issues/90
+
 -- TODO: Ensure _ALL_ keybinds work or are rebound
+
 -- TODO: Unfortunately the plugin does not respect the global codeium_enabled variable
+
 --[
 --Defaults
 -- Clear current suggestion     - codeium#Clear()               <C-]>
--- lua/zeronvim/utils/init.lua
+++ lua/zeronvim/utils/init.lua
@@ -3,6 +3,7 @@ local utils = {}
 utils.protected_plugin_call = require "zeronvim.utils.protected-plugin-call"
 utils.sourcing = require "zeronvim.utils.sourcing"
 utils.get_filename = require "zeronvim.utils.get-filename"
+utils.get_system_binary = require "zeronvim.utils.get-system-bianry"
 utils.get_file_extension = require "zeronvim.utils.get-file-extension"
 utils.is_input_empty = require "zeronvim.utils.is-input-empty"
 utils.get_buf_option = require "zeronvim.utils.get-buf-option"
