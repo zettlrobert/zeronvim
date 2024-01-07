67532b5815bd896bdbe0e91fb9cf06a6dcf44b80
zettlrobert <zettl.robert@gmail.com>
2024-01-07 01:46:01 +0100

    feat(lualine): add plugin

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,71 @@
+-- https://github.com/nvim-lualine/lualine.nvim
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    local lualine = require('lualine')
+
+    local hide_in_width = function()
+      return vim.fn.winwidth(0) > 80
+    end
+
+    local diagnostics = {
+      "diagnostics",
+      sources = { "nvim_diagnostic" },
+      sections = { "error", "warn" },
+      symbols = { error = " ", warn = " " },
+      colored = false,
+      update_in_insert = false,
+      always_visible = true,
+    }
+
+    local diff = {
+      "diff",
+      colored = false,
+      symbols = { added = " ", modified = " ", removed = " " },
+      cond = hide_in_width
+    }
+
+    lualine.setup({
+      options = {
+        icons_enabled = true,
+        theme = 'auto',
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+        disabled_filetypes = {
+          statusline = {},
+          winbar = {},
+        },
+        ignore_focus = {},
+        always_divide_middle = true,
+        globalstatus = true,
+        refresh = {
+          statusline = 1000,
+          tabline = 1000,
+          winbar = 1000,
+        }
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', diff, diagnostics },
+        lualine_c = { 'filename', 'searchcount' },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'windows', 'tabs', 'progress' },
+        lualine_z = { 'location' }
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
+        lualine_y = {},
+        lualine_z = {}
+      },
+      tabline = {},
+      winbar = {},
+      inactive_winbar = {},
+      extensions = {}
+
+    })
+  end
+}
