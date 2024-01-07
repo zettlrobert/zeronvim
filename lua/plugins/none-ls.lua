44f2330daf2c6af57cacd352dab649288cb3bc58
zettlrobert <zettl.robert@gmail.com>
2024-01-07 18:56:01 +0100

    feat: setup none-ls, null-ls replacement for formatting

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,30 @@
+-- https://github.com/nvimtools/none-ls.nvim
+return {
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require('null-ls')
+
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting['prettier'].with({
+          extra_args = function()
+            return {
+              "--printWidth=80",
+              "--single-quote",
+              "--trailing-comma=all",
+              "--bracket-spacing=true",
+              "--bracket-sameLine=false",
+              "--semi",
+              "--useTabs=false",
+              "--tabWidth=2",
+              "--arrowParens=always"
+            }
+          end
+        })
+      }
+    })
+
+    vim.keymap.set('n', '<space>f', vim.lsp.buf.format)
+  end
+}
