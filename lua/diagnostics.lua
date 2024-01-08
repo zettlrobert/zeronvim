705d163baffbcbb347f47ad6ef5bcaef84679cbc
zettlrobert <zettl.robert@gmail.com>
2024-01-08 02:11:38 +0100

    feat: diagnostics shortcuts

--- lua/diagnostics.lua
+++ lua/diagnostics.lua
@@ -1,4 +1,5 @@
 -- Global Diagnostics Keybinds
 vim.keymap.set('n', '<space>d', vim.diagnostic.open_float)
-vim.keymap.set('n', '<space>d', vim.diagnostic.open_float)
-vim.keymap.set('n', '<space>d', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
