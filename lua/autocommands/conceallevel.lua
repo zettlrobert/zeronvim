645a4f81ab6d85d0e38a085ce48a7666ea368a0e
zettlrobert <zettl.robert@gmail.com>
2024-02-10 11:42:14 +0100

    feat: json conceallevel

--- lua/autocommands/conceallevel.lua
+++ lua/autocommands/conceallevel.lua
@@ -1,5 +1,13 @@
 -- Conceal Syntax (pretty rendering)
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
   pattern = { "*.norg", "*.md" },
-  command = "set conceallevel=3"
+  command = "set conceallevel=3",
+  group = vim.api.nvim_create_augroup("write_conceal", { clear = true }),
+})
+
+-- JSON files should not be concealled
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+  pattern = { "*.json" },
+  command = "set conceallevel=0",
+  group = vim.api.nvim_create_augroup("json_conceal", { clear = true }),
 })
