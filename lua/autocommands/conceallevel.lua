8c96cdbdb13d8fe8ab092f6a1ab6c9616904a3af
zettlrobert <zettl.robert@gmail.com>
2024-07-18 23:01:19 +0200

    refactor: conceallevel for markdown files

--- lua/autocommands/conceallevel.lua
+++ lua/autocommands/conceallevel.lua
@@ -1,8 +1,14 @@
 -- Conceal Syntax (pretty rendering)
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
-  pattern = { "*.norg", "*.md" },
+  pattern = { "*.norg" },
   command = "set conceallevel=3",
   group = vim.api.nvim_create_augroup("write_conceal", { clear = true }),
+})
+
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+  pattern = { "*.md"},
+  command = "set conceallevel=0",
+  group = vim.api.nvim_create_augroup("markdown_no_conceal", { clear = true})
 })
 
 -- JSON files should not be concealled
