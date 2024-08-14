f6c32673e1889a3d2ee4cc474c48f3370afb1898
zettlrobert <zettl.robert@gmail.com>
2024-08-14 17:22:02 +0200

    refactor: stop using neorg

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -1,30 +1,35 @@
-return {
-  "nvim-neorg/neorg",
-  tag = "v7.0.0",
-  build = ":Neorg sync-parsers",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    require("neorg").setup {
-      load = {
-        ["core.defaults"] = {},   -- Loads default behaviour
-        ["core.concealer"] = {
-          config = {
-            icon_preset = 'diamond',
-          },
-        },                    -- Adds pretty icons to your documents
-        ["core.dirman"] = {   -- Manages Neorg workspaces
-          config = {
-            workspaces = {
-              zettlrobert = "~/notes/zettl",
-              zerodev = "~/notes/zerodev-solutions",
-              mobilehead = "~/notes/mobilehead",
-            },
-            default_workspace = "zettlrobert",
-          }
-        },
-        ["core.summary"] = {},
-        ["core.export"] = {},
-      },
-    }
-  end,
-}
+return {}
+--   "nvim-neorg/neorg",
+--   tag = "v7.0.0",
+--   build = ":Neorg sync-parsers",
+--   dependencies = { "nvim-lua/plenary.nvim" },
+--   config = function()
+--     require("neorg").setup({
+--       load = {
+--         ["core.defaults"] = {}, -- Loads default behaviour
+--         ["core.concealer"] = {
+--           config = {
+--             icon_preset = "diamond",
+--           },
+--         }, -- Adds pretty icons to your documents
+--         ["core.dirman"] = { -- Manages Neorg workspaces
+--           config = {
+--             workspaces = {
+--               zettlrobert = "~/notes/zettl",
+--               zerodev = "~/notes/zerodev-solutions",
+--               mobilehead = "~/notes/mobilehead",
+--             },
+--             default_workspace = "zettlrobert",
+--           },
+--         },
+--         ["core.summary"] = {},
+--         ["core.export"] = {
+--           export_dir = "~/Downloads/",
+--           config = {
+--             extensions = { "all" },
+--           },
+--         },
+--       },
+--     })
+--   end,
+-- }
