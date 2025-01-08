760243c5bb009b57a11afd0d8a180f5a37a7fc37
zettlrobert <zettl.robert@gmail.com>
2025-01-08 14:17:56 +0100

    feat: add fidget for lsp loading notifications

---
+++ lua/config/plugins/fidget.lua
@@ -0,0 +1,9 @@
+--https://github.com/j-hui/fidget.nvim
+return {
+  "https://github.com/j-hui/fidget.nvim",
+  config = function()
+    local fidget = require("fidget")
+
+    fidget.setup({})
+  end
+}
