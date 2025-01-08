38d9f728e3f029f5894ebf124e6eb86560692b60
zettlrobert <zettl.robert@gmail.com>
2025-01-08 01:22:23 +0100

    feat: format on save via lsp

---
+++ lua/config/formatting.lua
@@ -0,0 +1,20 @@
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+    if not client then
+      return
+    end
+
+    -- @diagnostic disable-next-line
+    if client.supports_method("textDocument/formatting") then
+      -- Format the current buffer on safe
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        buffer = args.buf,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+        end,
+      })
+    end
+  end,
+})
