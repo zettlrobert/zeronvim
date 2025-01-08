8b1707d07760b0756bfe0581ccd08a63eac14528
zettlrobert <zettl.robert@gmail.com>
2025-01-08 19:24:13 +0100

    feat: add icons for nvim tree and the respective diagnostics

--- lua/config/assets/icons.lua
+++ lua/config/assets/icons.lua
@@ -2,4 +2,36 @@ local M = {}
 
 M.titlestring = " "
 
+M.diagnostics = {
+  ERROR = "",
+  WARN = "",
+  HINT = "",
+  INFO = "",
+}
+
+M.Tree = {
+  default = "",
+  symlink = "",
+  bookmark = "",
+  symlink_arrow = " ➛ ",
+  Folder = {
+    ARROW_CLOSED = "",
+    ARROW_OPEN = "",
+    DEFAULT = "",
+    OPEN = "",
+    EMPTY = "",
+    EMPTY_OPEN = "",
+    SYMLINK = "",
+  },
+  Git = {
+    unstaged = "✗",
+    staged = "✓",
+    unmerged = "",
+    renamed = "➜",
+    untracked = "★",
+    deleted = "",
+    ignored = "◌",
+  },
+}
+
 return M
