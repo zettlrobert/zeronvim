d5998eede78c3584d2fb6f6f458ce5c731a5041b
zettlrobert <zettl.robert@gmail.com>
2025-01-08 03:30:52 +0100

    refactor: add mini icons to oil

--- lua/config/plugins/oil.lua
+++ lua/config/plugins/oil.lua
@@ -1,6 +1,11 @@
 return {
   {
     "stevearc/oil.nvim",
+    dependencies = {
+      {
+        "echasnovski/mini.icons", opts = {}
+      }
+    },
     config = function()
       local oil = require("oil")
 
