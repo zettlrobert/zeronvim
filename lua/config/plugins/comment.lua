c856af75eb55001150b987de8a1b3cf444b06b7a
zettlrobert <zettl.robert@gmail.com>
2025-01-09 16:40:04 +0100

    refactor: add missing option in comment setup

--- lua/config/plugins/comment.lua
+++ lua/config/plugins/comment.lua
@@ -5,7 +5,30 @@ return {
     local comment = require("Comment")
 
     comment.setup({
-      pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook()
+      pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+      padding = true,
+      sticky = true,
+      ignore = 'nil',
+      mappings = {
+        basic = true,
+        extra = true
+      },
+      toggler = {
+        block = 'gbc',
+        line = 'gcc'
+      },
+      post_hook = function()
+        return
+      end,
+      extra = {
+        above = 'gcO',
+        below = 'gco',
+        eol = 'gcA'
+      },
+      opleader = {
+        block = 'gb',
+        line = 'gc'
+      }
     })
   end
 }
