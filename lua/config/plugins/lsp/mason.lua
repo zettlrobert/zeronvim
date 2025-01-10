c4bf421f1117af7f0f53d7b67e91a7d7c1f200a1
zettlrobert <zettl.robert@gmail.com>
2025-01-10 15:17:34 +0100

    refactor: add mason icons and keymaps

--- lua/config/plugins/lsp/mason.lua
+++ lua/config/plugins/lsp/mason.lua
@@ -6,7 +6,7 @@ return {
     "williamboman/mason.nvim",
     dependencies = {
       "williamboman/mason-lspconfig.nvim",
-      "WhoIsSethDaniel/mason-tool-installer.nvim"
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
     },
     config = function()
       local mason = require("mason")
@@ -15,6 +15,7 @@ return {
       local language_servers = require("config.plugins.lsp.servers").language_servers
       local formatters = require("config.plugins.lsp.servers").formatters
       local debug_adapters = require("config.plugins.lsp.servers").debug_adapters
+      local icons = require("config.assets.icons")
 
       -- LSP
       local mason_servers = {}
@@ -22,13 +23,32 @@ return {
         table.insert(mason_servers, key)
       end
 
-      mason.setup({})
+      mason.setup({
+        ui = {
+          icons = {
+            package_installed = icons.Mason.package_installed,
+            package_pending = icons.Mason.package_pending,
+            package_uninstalled = icons.Mason.package_uninstalled,
+          },
+        },
+        keymaps = {
+          toggle_package_expand = "<CR>",
+          install_package = "i",
+          update_package = "u",
+          check_package_version = "c",
+          update_all_packages = "U",
+          check_outdated_packages = "C",
+          uninstall_package = "X",
+          cancel_installation = "<C-c>",
+          apply_language_filter = "<C-f>",
+        },
+      })
+
       mason_lspconfig.setup({
         ensure_installed = mason_servers,
 
         automatic_installation = false,
       })
-
 
       -- Formatters & Debug Adapters
       local mason_tools = {}
@@ -41,7 +61,7 @@ return {
       end
 
       mason_tool_installer.setup({
-        ensure_installed = mason_tools
+        ensure_installed = mason_tools,
       })
     end,
   },
