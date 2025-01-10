3c36aa59a733a4682178302132ad3c9cf097a2e0
zettlrobert <zettl.robert@gmail.com>
2025-01-10 01:14:25 +0100

    feat: add colorpicker ccc

---
+++ lua/config/plugins/colorpicker.lua
@@ -0,0 +1,18 @@
+-- https://github.com/uga-rosa/ccc.nvim
+return {
+  "uga-rosa/ccc.nvim",
+  config = function()
+    local ccc = require("ccc")
+
+    ccc.setup({
+      highlighter = {
+        auto_enable = true,
+        lsp = true,
+      },
+    })
+
+    vim.keymap.set("n", "<leader>ccc", function()
+      vim.api.nvim_command("CccPick")
+    end, { desc = ":Toggle CccPick - Colorpicker" })
+  end,
+}
