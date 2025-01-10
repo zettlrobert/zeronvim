a37f4da754169dd3fd3aafb67cd070c268ce768d
zettlrobert <zettl.robert@gmail.com>
2025-01-10 01:14:44 +0100

    feat: add obsidian
    configuration still pending

---
+++ lua/config/plugins/obsidian.lua
@@ -0,0 +1,51 @@
+--https://github.com/epwalsh/obsidian.nvim
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*",
+  lazy = true,
+  ft = "markdown",
+  opts = {
+    workspaces = {
+      {
+        name = "zettl",
+        path = "~/notes/zettl",
+      },
+    },
+    ui = {
+      checkboxes = {},
+      bullets = {},
+    },
+    -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
+    completion = {
+      -- Set to false to disable completion.
+      nvim_cmp = true,
+      -- Trigger completion at 2 chars.
+      min_chars = 2,
+    },
+    -- Optional, configure key mappings. These are the defaults. If you don't want to set any keymappings this
+    -- way then set 'mappings = {}'.
+    mappings = {
+      -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+      ["gf"] = {
+        action = function()
+          return require("obsidian").util.gf_passthrough()
+        end,
+        opts = { noremap = false, expr = true, buffer = true },
+      },
+      -- Toggle check-boxes.
+      ["<leader>ch"] = {
+        action = function()
+          return require("obsidian").util.toggle_checkbox()
+        end,
+        opts = { buffer = true },
+      },
+      -- Smart action depending on context, either follow link or toggle checkbox.
+      ["<cr>"] = {
+        action = function()
+          return require("obsidian").util.smart_action()
+        end,
+        opts = { buffer = true, expr = true },
+      },
+    },
+  },
+}
