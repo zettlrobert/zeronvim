51b003ddf25688c8a64a641eeb59aba11528f8ca
zettlrobert <zettl.robert@gmail.com>
2025-01-10 13:22:23 +0100

    style: update formatting

--- lua/config/ui/lsp-hover.lua
+++ lua/config/ui/lsp-hover.lua
@@ -1,5 +1,7 @@
+--[[
 -- BUG: This is broken
 -- Ensure Hover Doc has rounded border
+ ]]
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.buf.hover({
-  border = "rounded"
+  border = "rounded",
 })
