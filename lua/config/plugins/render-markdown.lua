913725d7bd576e161f1590381c7b107783f19f6a
zettlrobert <zettl.robert@gmail.com>
2025-01-10 01:00:52 +0100

    feat: add render-markdown and markdown completions

---
+++ lua/config/plugins/render-markdown.lua
@@ -0,0 +1,12 @@
+-- https://github.com/MeanderingProgrammer/render-markdown.nvim
+-- https://github.com/MeanderingProgrammer/render-markdown.nvim/wiki
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+  config = function()
+    local markdown = require("render-markdown")
+    markdown.setup({
+      render_modes = true,
+    })
+  end,
+}
