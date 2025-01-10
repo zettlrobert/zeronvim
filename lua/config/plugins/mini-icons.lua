513c08848e20d3ad304e841d51dc93b61e70d1d8
zettlrobert <zettl.robert@gmail.com>
2025-01-10 15:18:19 +0100

    feat: add mini icons and mock nvim_web_devicons

---
+++ lua/config/plugins/mini-icons.lua
@@ -0,0 +1,12 @@
+--https://github.com/echasnovski/mini.icons
+return {
+  "echasnovski/mini.icons",
+  version = "*",
+  config = function()
+    local mini_icons = require("mini.icons")
+
+    mini_icons.setup({})
+
+    mini_icons.mock_nvim_web_devicons()
+  end,
+}
