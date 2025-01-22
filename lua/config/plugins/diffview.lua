ba23cfe4aba7b2e0721e01b444b0e0eedfd80855
zettlrobert <zettl.robert@gmail.com>
2025-01-22 19:31:53 +0100

    feat: setup diffview

---
+++ lua/config/plugins/diffview.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "sindrets/diffview.nvim",
+    config = function()
+      local diffview = require("diffview")
+
+      print("Diffview Setup")
+
+      diffview.setup({})
+
+      vim.keymap.set("n", "<leader>gD", ":DiffviewOpen<CR>", { desc = "Diffview: Open Diffview" })
+      vim.keymap.set("n", "<leader>gC", ":DiffviewClose<CR>", { desc = "Diffview: Close Diffview" })
+      -- vim.keymap.set("n", "<leader>gP", ":DiffviewToggleFiles<CR>", { desc = "Diffview: Toggle Diffview Files" })
+    end,
+  },
+}
