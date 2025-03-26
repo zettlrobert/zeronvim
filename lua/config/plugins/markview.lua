9b215d6613351bcb2dc19d82bf736c1e4e434fa4
zettlrobert <zettl.robert@gmail.com>
2025-03-26 22:26:51 +0100

    feat: boilerplate markview

---
+++ lua/config/plugins/markview.lua
@@ -0,0 +1,9 @@
+-- https://github.com/OXY2DEV/markview.nvim
+return {}
+-- return {
+--   "https://github.com/OXY2DEV/markview.nvim",
+--   lazy = false,
+--   dependencies = {
+--     "saghen/blink.cmp"
+--   }
+-- }
