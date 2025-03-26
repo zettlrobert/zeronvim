37c6e926b8017435d781dcd95dfab50f6b684155
zettlrobert <zettl.robert@gmail.com>
2025-03-26 22:40:49 +0100

    fix: none-ls update fixes depcreated method

--- lua/config/plugins/none-ls.lua
+++ lua/config/plugins/none-ls.lua
@@ -1,5 +1,6 @@
 -- https://github.com/nvimtools/none-ls.nvim
 -- https://github.com/nvimtools/none-ls-extras.nvim
+
 return {
   "nvimtools/none-ls.nvim",
   dependencies = {
@@ -8,35 +9,36 @@ return {
   config = function()
     local null_ls = require("null-ls")
 
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting["stylua"].with({
-          extra_args = function()
-            return {
-              "--indent-type=Spaces",
-              "--indent-width=2",
-            }
-          end,
-        }),
-        null_ls.builtins.formatting["prettierd"].with({
-          extra_args = function()
-            return {
-              "--printWidth=80",
-              "--single-quote",
-              "--trailing-comma=all",
-              "--bracket-spacing=true",
-              "--bracket-sameLine=false",
-              "--semi",
-              "--useTabs=false",
-              "--tabWidth=2",
-              "--arrowParens=always",
-            }
-          end,
-        }),
+    return
+        null_ls.setup({
+          sources = {
+            null_ls.builtins.formatting["stylua"].with({
+              extra_args = function()
+                return {
+                  "--indent-type=Spaces",
+                  "--indent-width=2",
+                }
+              end,
+            }),
+            null_ls.builtins.formatting["prettierd"].with({
+              extra_args = function()
+                return {
+                  "--printWidth=80",
+                  "--single-quote",
+                  "--trailing-comma=all",
+                  "--bracket-spacing=true",
+                  "--bracket-sameLine=false",
+                  "--semi",
+                  "--useTabs=false",
+                  "--tabWidth=2",
+                  "--arrowParens=always",
+                }
+              end,
+            }),
 
-        -- Vale
-        -- null_ls.builtins.diagnostics.vale,
-      },
-    })
+            -- Vale
+            -- null_ls.builtins.diagnostics.vale,
+          },
+        })
   end
 }
