b44b554392c8be7fafa4ce3c7eb1f65f94ab66f7
zettlrobert <zettl.robert@gmail.com>
2025-06-17 01:12:20 +0200

    style: auto format

--- lua/config/plugins/render-markdown.lua
+++ lua/config/plugins/render-markdown.lua
@@ -4,17 +4,35 @@ return {
   "MeanderingProgrammer/render-markdown.nvim",
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
-    "nvim-tree/nvim-web-devicons"
+    "nvim-tree/nvim-web-devicons",
   },
   config = function()
     local markdown = require("render-markdown")
+
+    local code_block = "#222235"
+
+    vim.api.nvim_set_hl(0, "CodeBlock", { bg = code_block })
+
     markdown.setup({
+      enabled = true,
+      preset = "obsidian",
       render_modes = true,
       completions = {
         blink = {
-          enabled = true
-        }
-      }
+          enabled = true,
+        },
+      },
+      heading = {
+        sign = false,
+        width = "block",
+        min_width = 120,
+      },
+      code = {
+        position = "right",
+        width = "block",
+        border = "thick",
+        highlight = "CodeBlock",
+      },
     })
   end,
 }
