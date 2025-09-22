c2ce751274a8d30080d01e8b3abd69d2f31742fe
Zettl Robert <zettl.robert@gmail.com>
2025-09-22 19:15:36 +0200

    feat: add keybind to toggle current lineblmae

--- lua/config/plugins/gitsigns.lua
+++ lua/config/plugins/gitsigns.lua
@@ -7,9 +7,13 @@ return {
       gitsigns.setup()
 
       vim.keymap.set("n", "<space>gsw", gitsigns.toggle_word_diff, { desc = "Gitsigns toggle word_diff" })
-      vim.keymap.set("n", "<space>blame", gitsigns.toggle_current_line_blame,
-        { desc = "Gitsigns toggle_current_line_blame" })
+      vim.keymap.set(
+        "n",
+        "<space>blame",
+        gitsigns.toggle_current_line_blame,
+        { desc = "Gitsigns toggle_current_line_blame" }
+      )
       vim.keymap.set("n", "<space>gdiff", gitsigns.diffthis, { desc = "Gitsigns diffthis" })
-    end
-  }
+    end,
+  },
 }
