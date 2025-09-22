ac026f016c45918412fc24ac654cbf37eb401a5e
Zettl Robert <zettl.robert@gmail.com>
2025-09-22 19:17:36 +0200

    feat: set colorscheme

--- init.lua
+++ init.lua
@@ -8,7 +8,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out,                            "WarningMsg" },
+      { out, "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -31,6 +31,8 @@ require("lazy").setup({
     { import = "config.plugins" },
   },
 })
+
+vim.cmd("colorscheme catppuccin-mocha")
 
 -- Require Neovim Options
 require("config.options")
