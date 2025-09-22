c8ebc899023a71379965f8629e120a79ce464ba7
Zettl Robert <zettl.robert@gmail.com>
2025-09-22 19:18:01 +0200

    feat: add minidiff for codecompanion

---
+++ lua/config/plugins/mini_diff.lua
@@ -0,0 +1,10 @@
+---https://github.com/echasnovski/mini.diff
+return {
+  "echasnovski/mini.diff",
+  config = function()
+    local mini_diff = require("mini.diff")
+
+    ---https://github.com/echasnovski/mini.diff?tab=readme-ov-file#default-config
+    mini_diff.setup()
+  end,
+}
