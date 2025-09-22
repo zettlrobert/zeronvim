df9e777d7129b2c09ddc523e29229f84b5c0fc3a
Zettl Robert <zettl.robert@gmail.com>
2025-09-22 19:17:12 +0200

    feat: add markdown formatting filter for prsentation files via presentterm

--- lua/config/utils/is_presentation_markdown.lua
+++ lua/config/utils/is_presentation_markdown.lua
@@ -7,7 +7,7 @@ Used to skip formatting and set conceallevel for *.presentation.md files.
 local is_presentation_markdown = function()
   local filename = vim.fn.expand("%")
 
-  if filename:match(".*presentation.md") then
+  if filename:match("*.presentation.md") then
     vim.opt.conceallevel = 1
     return true
   else
