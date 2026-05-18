0f3704c7737132a5cadd90f11e9d7462e12fbe9c
Zettl Robert <zettl.robert@gmail.com>
2026-05-18 10:48:36 +0200

    fix: vtsls cmd argument splitting and add nx monorepo root markers

--- lsp/vtsls.lua
+++ lsp/vtsls.lua
@@ -1,18 +1,36 @@
---- check for vtsls binary
+-- vtsls (TypeScript / JavaScript LSP wrapping tsserver)
+-- https://github.com/yioneko/vtsls
+-- Settings: https://github.com/yioneko/vtsls/blob/main/packages/service/configuration.schema.json
+-- :h lsp-config
+
 local function is_vtsls_installed()
-	return vim.fn.executable("vtsls") == 1
+  return vim.fn.executable("vtsls") == 1
 end
 
 if is_vtsls_installed() == false then
-	vim.notify(
-		"vtsls is not installed. Please install it to enable the vtsls LSP server. https://github.com/yioneko/vtsls",
-		vim.log.levels.ERROR
-	)
-	return
+  vim.notify(
+    "vtsls is not installed. Please install it to enable the vtsls LSP server. https://github.com/yioneko/vtsls",
+    vim.log.levels.ERROR
+  )
+  return
 end
 
 return {
-	cmd = { "vtsls --stdio" },
-	root_markers = { ".git" },
-	filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
+  cmd = { "vtsls", "--stdio" },
+  -- nx monorepo: tsconfig.base.json + nx.json mark the workspace root;
+  -- package.json keeps it working for non-nx projects
+  root_markers = {
+    "tsconfig.base.json",
+    "nx.json",
+    "package.json",
+    ".git",
+  },
+  filetypes = {
+    "javascript",
+    "javascriptreact",
+    "javascript.jsx",
+    "typescript",
+    "typescriptreact",
+    "typescript.tsx",
+  },
 }
