2e15a0f4aa9ed5f93881adc23cbc91db14bdbf4b
Zettl Robert <zettl.robert@gmail.com>
2026-05-20 16:01:17 +0200

    feat: add per-window heading for quickfix and location list windows

--- init.lua
+++ init.lua
@@ -54,5 +54,8 @@ require("config.utils")
 -- Require window-title
 require("config.window-title")
 
+-- Require quickfix/loclist window heading
+require("config.qf_heading")
+
 -- Require spellcheck
 require("config.spellcheck")
