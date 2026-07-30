1235ff1a19e4290bdf58f4598b0c66d246199b41
Zettl Robert <zettl.robert@gmail.com>
2026-07-30 14:13:07 +0200

    refactor: migrate flash.lua to keymap_desc util

--- lua/config/plugins/flash.lua
+++ lua/config/plugins/flash.lua
@@ -4,16 +4,19 @@
 -- ambiguous matches) and `/`/`?` (search with live labels). Treesitter mode
 -- (`S`) selects whole syntactic nodes.
 
+local kd = require("config.utils.keymap_desc")
+local K, T = kd.KIND, kd.TOOL
+
 return {
   "folke/flash.nvim",
   event = "VeryLazy",
   opts = {},
   -- stylua: ignore
   keys = {
-    { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash: jump" },
-    { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash: treesitter" },
-    { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Flash: remote (operator)" },
-    { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Flash: treesitter search" },
-    { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Flash: toggle in cmdline search" },
+    { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = kd.format(K.JUMP, T.Flash, "hint-based") },
+    { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = kd.format(K.JUMP, T.Flash, "treesitter node") },
+    { "r",     mode = "o",               function() require("flash").remote() end,            desc = kd.format(K.JUMP, T.Flash, "remote (operator)") },
+    { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = kd.format(K.SEARCH, T.Flash, "treesitter") },
+    { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = kd.format(K.TOGGLE, T.Flash, "in cmdline search") },
   },
 }
