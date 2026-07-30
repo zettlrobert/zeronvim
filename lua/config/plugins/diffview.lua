34a83f188fda68f8d83ea926bf454790ff892c55
Zettl Robert <zettl.robert@gmail.com>
2026-07-30 14:10:40 +0200

    refactor: migrate diffview.lua to keymap_desc util

--- lua/config/plugins/diffview.lua
+++ lua/config/plugins/diffview.lua
@@ -3,12 +3,14 @@ return {
     "sindrets/diffview.nvim",
     config = function()
       local diffview = require("diffview")
+      local kd = require("config.utils.keymap_desc")
+      local K, T = kd.KIND, kd.TOOL
 
       ---TODO: Configure Diffview
       diffview.setup({})
 
-      vim.keymap.set("n", "<leader>gD", ":DiffviewOpen<CR>", { desc = "Diffview: open" })
-      vim.keymap.set("n", "<leader>gC", ":DiffviewClose<CR>", { desc = "Diffview: close" })
+      vim.keymap.set("n", "<leader>gD", ":DiffviewOpen<CR>", { desc = kd.format(K.OPEN, T.Diffview, "diff view") })
+      vim.keymap.set("n", "<leader>gC", ":DiffviewClose<CR>", { desc = kd.format(K.CLOSE, T.Diffview, "diff view") })
       -- vim.keymap.set("n", "<leader>gP", ":DiffviewToggleFiles<CR>", { desc = "Diffview: Toggle Diffview Files" })
     end,
   },
