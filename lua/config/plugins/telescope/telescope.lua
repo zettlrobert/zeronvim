150411948ed034d0b7a138bc90f2d916bfac9284
Zettl Robert <zettl.robert@gmail.com>
2026-07-30 14:10:13 +0200

    refactor: migrate telescope.lua to keymap_desc util

--- lua/config/plugins/telescope/telescope.lua
+++ lua/config/plugins/telescope/telescope.lua
@@ -9,6 +9,8 @@ return {
   config = function()
     local telescope = require("telescope")
     local telescope_builtin = require("telescope.builtin")
+    local kd = require("config.utils.keymap_desc")
+    local K, T = kd.KIND, kd.TOOL
 
     telescope.setup({
       pickers = {
@@ -35,10 +37,14 @@ return {
     telescope.load_extension("fzf")
 
     -- Help tags
-    vim.keymap.set("n", "<leader>fh", telescope_builtin.help_tags, { desc = "Telescope: help tags" })
+    vim.keymap.set("n", "<leader>fh", telescope_builtin.help_tags, {
+      desc = kd.format(K.PICK, T.Telescope, "help tags"),
+    })
 
     -- Telescope find files
-    vim.keymap.set("n", "<leader>ff", telescope_builtin.find_files, { desc = "Telescope: files" })
+    vim.keymap.set("n", "<leader>ff", telescope_builtin.find_files, {
+      desc = kd.format(K.PICK, T.Telescope, "files"),
+    })
 
     -- Telescope hidden find files
     vim.keymap.set("n", "<leader>f.", function()
@@ -46,35 +52,47 @@ return {
         find_command = { "rg", "--hidden", "--files" },
         theme = "ivy",
       })
-    end, { desc = "Telescope: files (including hidden)" })
+    end, { desc = kd.format(K.PICK, T.Telescope, "files (including hidden)") })
 
     -- Search all open buffers
-    vim.keymap.set("n", "<leader>fb", telescope_builtin.buffers, { desc = "Telescope: buffers" })
+    vim.keymap.set("n", "<leader>fb", telescope_builtin.buffers, {
+      desc = kd.format(K.PICK, T.Telescope, "buffers"),
+    })
 
     -- Live Grep
-    vim.keymap.set("n", "<leader>fl", telescope_builtin.live_grep, { desc = "Telescope: live grep" })
+    vim.keymap.set("n", "<leader>fl", telescope_builtin.live_grep, {
+      desc = kd.format(K.SEARCH, T.Telescope, "live grep"),
+    })
 
     -- LSP pickers (results can be sent to qflist via <C-q> inside the picker)
-    vim.keymap.set("n", "<leader>fr", telescope_builtin.lsp_references, { desc = "Telescope: LSP references" })
-    vim.keymap.set("n", "<leader>fd", telescope_builtin.lsp_definitions, { desc = "Telescope: LSP definitions" })
-    vim.keymap.set("n", "<leader>fD", telescope_builtin.diagnostics, { desc = "Telescope: diagnostics" })
+    vim.keymap.set("n", "<leader>fr", telescope_builtin.lsp_references, {
+      desc = kd.format(K.PICK, T.Telescope, "LSP references"),
+    })
+    vim.keymap.set("n", "<leader>fd", telescope_builtin.lsp_definitions, {
+      desc = kd.format(K.PICK, T.Telescope, "LSP definitions"),
+    })
+    vim.keymap.set("n", "<leader>fD", telescope_builtin.diagnostics, {
+      desc = kd.format(K.PICK, T.Telescope, "diagnostics"),
+    })
 
     -- Keymap picker (complements which-key: search by desc)
-    vim.keymap.set("n", "<leader>fk", telescope_builtin.keymaps, { desc = "Telescope: keymaps" })
+    vim.keymap.set("n", "<leader>fk", telescope_builtin.keymaps, {
+      desc = kd.format(K.PICK, T.Telescope, "keymaps"),
+    })
 
     -- Open the neovim config files — <leader>f namespace (find)
     vim.keymap.set("n", "<leader>fn", function()
       require("telescope.builtin").find_files({
         cwd = vim.fn.stdpath("config"),
       })
-    end, { desc = "Telescope: nvim config files" })
+    end, { desc = kd.format(K.PICK, T.Telescope, "nvim config files") })
 
     -- Search files of installed neovim plugins
     vim.keymap.set("n", "<leader>fp", function()
       require("telescope.builtin").find_files({
         cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy"),
       })
-    end, { desc = "Telescope: installed plugin files" })
+    end, { desc = kd.format(K.PICK, T.Telescope, "installed plugin files") })
 
     -- Setup Multigrep
     require("config.plugins.telescope.multigrep").setup()
