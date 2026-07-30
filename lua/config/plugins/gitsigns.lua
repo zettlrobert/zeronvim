a048a6bf499449b0bb7d44a359f71cd3b99d103f
Zettl Robert <zettl.robert@gmail.com>
2026-07-30 14:10:52 +0200

    refactor: migrate gitsigns.lua to keymap_desc util

--- lua/config/plugins/gitsigns.lua
+++ lua/config/plugins/gitsigns.lua
@@ -3,13 +3,21 @@ return {
     "lewis6991/gitsigns.nvim",
     config = function()
       local gitsigns = require("gitsigns")
+      local kd = require("config.utils.keymap_desc")
+      local K, T = kd.KIND, kd.TOOL
 
       gitsigns.setup()
 
       -- <leader>g namespace (git)
-      vim.keymap.set("n", "<leader>gw", gitsigns.toggle_word_diff, { desc = "Gitsigns: toggle word diff" })
-      vim.keymap.set("n", "<leader>gb", gitsigns.toggle_current_line_blame, { desc = "Gitsigns: toggle current-line blame" })
-      vim.keymap.set("n", "<leader>gh", gitsigns.diffthis, { desc = "Gitsigns: hunk diff current file" })
+      vim.keymap.set("n", "<leader>gw", gitsigns.toggle_word_diff, {
+        desc = kd.format(K.TOGGLE, T.Gitsigns, "word diff"),
+      })
+      vim.keymap.set("n", "<leader>gb", gitsigns.toggle_current_line_blame, {
+        desc = kd.format(K.TOGGLE, T.Gitsigns, "current-line blame"),
+      })
+      vim.keymap.set("n", "<leader>gh", gitsigns.diffthis, {
+        desc = kd.format(K.SHOW, T.Gitsigns, "hunk diff current file"),
+      })
     end,
   },
 }
